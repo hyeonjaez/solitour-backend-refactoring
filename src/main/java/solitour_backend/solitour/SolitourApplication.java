5f3fe2145a16c84b621b4f72ccf49d858f338fa9
Donghun Won <wonhun1225@gmail.com>
2024-06-09 15:59:37 +0900

    Init : 프로젝트 세팅

---
+++ src/main/java/solitour_backend/solitour/SolitourApplication.java
@@ -0,0 +1,13 @@
+package solitour_backend.solitour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolitourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolitourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/solitour_backend/solitour/SolitourApplicationTests.java
@@ -0,0 +1,13 @@
+package solitour_backend.solitour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolitourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
