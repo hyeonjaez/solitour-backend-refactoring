6ee27b9313d669091d65adff827ab68392864f75
hyeonjaez <jhjh0022@naver.com>
2024-06-20 14:57:21 +0900

    refactor: setter, 생성자 추가

--- src/main/java/solitour_backend/solitour/place/entity/Place.java
+++ src/main/java/solitour_backend/solitour/place/entity/Place.java
@@ -1,15 +1,19 @@
 package solitour_backend.solitour.place.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Getter
+@Setter
 @Table(name = "place")
 @NoArgsConstructor
+@AllArgsConstructor
 public class Place {
 
     @Id
