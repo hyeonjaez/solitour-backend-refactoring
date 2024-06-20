f9edc32d2ffea2e2e8a1cb786e3b1b12a4d555c4
hyeonjaez <jhjh0022@naver.com>
2024-06-20 15:01:37 +0900

    fix: 사용하지 않은 코드 삭제

--- src/main/java/solitour_backend/solitour/place/dto/mapper/PlaceMapper.java
+++ src/main/java/solitour_backend/solitour/place/dto/mapper/PlaceMapper.java
@@ -1,12 +1,11 @@
 package solitour_backend.solitour.place.dto.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 import solitour_backend.solitour.place.dto.response.PlaceResponse;
 import solitour_backend.solitour.place.entity.Place;
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR, uses = BigDecimalMapper.class)
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface PlaceMapper {
 
     PlaceResponse mapToPlaceResponse(Place place);
