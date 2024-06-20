62a644fd5d17ff6a6a10c1e2ca7ac49d9ca0c78b
hyeonjaez <jhjh0022@naver.com>
2024-06-20 15:04:37 +0900

    refactor: 사용하지 않은 import 문 정리

--- src/main/java/solitour_backend/solitour/place/repository/PlaceRepository.java
+++ src/main/java/solitour_backend/solitour/place/repository/PlaceRepository.java
@@ -3,7 +3,6 @@ package solitour_backend.solitour.place.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solitour_backend.solitour.place.entity.Place;
 
-import java.util.Optional;
 
 public interface PlaceRepository extends JpaRepository<Place, Long> {
 }
