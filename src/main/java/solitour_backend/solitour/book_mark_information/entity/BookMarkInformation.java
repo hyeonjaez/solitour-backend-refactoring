4869aec449f88191b6c23f7e2c73f02be14395bd
hyeonjaez <jhjh0022@naver.com>
2024-06-26 00:45:45 +0900

    feat: erd 수정으로 인한 BookMark -> BookMarkInformation

--- src/main/java/solitour_backend/solitour/book_mark_information/entity/BookMarkInformation.java
+++ src/main/java/solitour_backend/solitour/book_mark_information/entity/BookMarkInformation.java
@@ -1,31 +1,27 @@
-package solitour_backend.solitour.great.entity;
+package solitour_backend.solitour.book_mark_information.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import solitour_backend.solitour.gathering.entity.Gathering;
 import solitour_backend.solitour.information.entity.Information;
 import solitour_backend.solitour.user.entity.User;
 
 @Entity
 @Getter
-@Table(name = "great")
+@Table(name = "book_mark_information")
 @NoArgsConstructor
-@IdClass(GreatId.class)
-public class Great {
+public class BookMarkInformation {
     @Id
+    @Column(name = "book_mark_information_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Id
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "information_id")
     private Information information;
-
-    @Id
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "gathering_id")
-    private Gathering gathering;
 
 }
