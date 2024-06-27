90e25e60dd7c69ef93866dab1e8ec8bee29c8a43
hyeonjaez <jhjh0022@naver.com>
2024-06-27 18:19:32 +0900

    feat: tag request dto

---
+++ src/main/java/solitour_backend/solitour/tag/dto/request/TagRegisterRequest.java
@@ -0,0 +1,10 @@
+package solitour_backend.solitour.tag.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class TagRegisterRequest {
+    private String name;
+}
