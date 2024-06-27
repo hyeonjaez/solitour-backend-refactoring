b78baddf11aa3a88c388013287b4b3f3c73c6bed
hyeonjaez <jhjh0022@naver.com>
2024-06-27 18:19:42 +0900

    feat: tag mapper

---
+++ src/main/java/solitour_backend/solitour/tag/dto/mapper/TagMapper.java
@@ -0,0 +1,24 @@
+package solitour_backend.solitour.tag.dto.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+import solitour_backend.solitour.tag.dto.request.TagRegisterRequest;
+import solitour_backend.solitour.tag.dto.response.TagResponse;
+import solitour_backend.solitour.tag.entity.Tag;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface TagMapper {
+
+    @Mapping(target = "tagId", ignore = true)
+    Tag mapToTag(TagRegisterRequest tagRegisterRequest);
+
+    List<Tag> mapToTags(List<TagRegisterRequest> tagRegisterRequests);
+
+    @Mapping(source = "tagId", target = "id")
+    TagResponse mapToTagResponse(Tag tag);
+
+    List<TagResponse> mapToTagResponses(List<Tag> tags);
+}
