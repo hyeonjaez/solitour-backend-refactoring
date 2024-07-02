603783a65d76cea85709be74a46d86ec3a5b9810
hyeonjaez <jhjh0022@naver.com>
2024-07-02 23:45:49 +0900

    feat: informationRegisterRequest dto

--- src/main/java/solitour_backend/solitour/information/dto/request/InformationRegisterRequest.java
+++ src/main/java/solitour_backend/solitour/information/dto/request/InformationRegisterRequest.java
@@ -2,6 +2,7 @@ package solitour_backend.solitour.information.dto.request;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 import solitour_backend.solitour.place.dto.request.PlaceRegisterRequest;
 import solitour_backend.solitour.tag.dto.request.TagRegisterRequest;
 
@@ -10,27 +11,24 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class InformationRegisterRequest {
-    //information
     private String informationTitle;
     private String informationAddress;
     private String informationContent;
     private String informationTips;
 
-    //user
+
     private Long userId;
 
-    //place
     private PlaceRegisterRequest placeRegisterRequest;
 
-    //category
     private Long categoryId;
 
-    //zoneCategory
     private Integer zoneCategoryId;
 
-    //tag
     private List<TagRegisterRequest> tagRegisterRequests;
 
-    //TODO image 어떻게 해야할지
+    private MultipartFile thumbNailImage;
+
+    private List<MultipartFile> contentImages;
 
 }
