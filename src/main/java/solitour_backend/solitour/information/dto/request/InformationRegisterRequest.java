b04b669722199f70cf1f8edb9fd05526181dc4db
hyeonjaez <jhjh0022@naver.com>
2024-07-07 02:58:39 +0900

    feat: information register validation

--- src/main/java/solitour_backend/solitour/information/dto/request/InformationRegisterRequest.java
+++ src/main/java/solitour_backend/solitour/information/dto/request/InformationRegisterRequest.java
@@ -1,5 +1,9 @@
 package solitour_backend.solitour.information.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import solitour_backend.solitour.place.dto.request.PlaceRegisterRequest;
@@ -10,19 +14,32 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class InformationRegisterRequest {
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String informationTitle;
+
+    @NotBlank
+    @Size(min = 1, max = 20)
     private String informationAddress;
+
     private String informationContent;
+
     private String informationTips;
 
-
+    @NotNull
+    @Min(1)
     private Long userId;
 
+    @NotNull
     private PlaceRegisterRequest placeRegisterRequest;
 
+    @NotNull
+    @Min(1)
     private Long categoryId;
 
-    private Integer zoneCategoryId;
+    @NotNull
+    @Min(1)
+    private Long zoneCategoryId;
 
     private List<TagRegisterRequest> tagRegisterRequests;
 
