7c369a99dc67a711062c8b43812996592f871297
hyeonjaez <jhjh0022@naver.com>
2024-07-07 02:16:32 +0900

    feat: ZoneCategory ddl 문 수정으로 인한 수정 id type Integer -> Long parentZoneCategory 추가

--- src/main/java/solitour_backend/solitour/zone_category/dto/response/ZoneCategoryResponse.java
+++ src/main/java/solitour_backend/solitour/zone_category/dto/response/ZoneCategoryResponse.java
@@ -2,10 +2,12 @@ package solitour_backend.solitour.zone_category.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import solitour_backend.solitour.zone_category.entity.ZoneCategory;
 
 @Getter
 @AllArgsConstructor
 public class ZoneCategoryResponse {
-    private Integer id;
+    private Long id;
+    private ZoneCategory parentZoneCategory;
     private String name;
 }
