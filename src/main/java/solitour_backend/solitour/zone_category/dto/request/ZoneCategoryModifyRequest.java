31029bc4e6ca8dbeec47a2703ca00827ef85643a
hyeonjaez <jhjh0022@naver.com>
2024-07-07 02:33:27 +0900

    feat: ZoneCategory ddl 문 수정으로 인한 수정 id type Integer -> Long parentZoneCategory 추가

--- src/main/java/solitour_backend/solitour/zone_category/dto/request/ZoneCategoryModifyRequest.java
+++ src/main/java/solitour_backend/solitour/zone_category/dto/request/ZoneCategoryModifyRequest.java
@@ -1,5 +1,7 @@
 package solitour_backend.solitour.zone_category.dto.request;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -8,6 +10,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ZoneCategoryModifyRequest {
+    @Nullable
+    @Min(1)
+    private Long parentId;
+
     @NotBlank
     @Size(max = 20)
     private String name;
