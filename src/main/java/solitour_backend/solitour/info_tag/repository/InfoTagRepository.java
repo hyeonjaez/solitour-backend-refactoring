f6a033043e628968df256cebba88550a32bed03d
hyeonjaez <jhjh0022@naver.com>
2024-07-11 00:54:54 +0900

    feature: information id에 해당하는 InfoTag 데이터 List 반환

--- src/main/java/solitour_backend/solitour/info_tag/repository/InfoTagRepository.java
+++ src/main/java/solitour_backend/solitour/info_tag/repository/InfoTagRepository.java
@@ -3,5 +3,8 @@ package solitour_backend.solitour.info_tag.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import solitour_backend.solitour.info_tag.entity.InfoTag;
 
+import java.util.List;
+
 public interface InfoTagRepository extends JpaRepository<InfoTag, Long> {
+    List<InfoTag> findAllByInformationId(Long informationId);
 }
