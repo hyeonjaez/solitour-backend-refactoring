668e71d9baf7bef7777833af4576ac4343ed3f93
Donghun Won <wonhun1225@gmail.com>
2024-07-10 11:15:20 +0900

    Feat: cors 문제 해결

--- src/main/java/solitour_backend/solitour/auth/config/AuthConfiguration.java
+++ src/main/java/solitour_backend/solitour/auth/config/AuthConfiguration.java
@@ -4,6 +4,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -35,5 +36,14 @@ public class AuthConfiguration implements WebMvcConfigurer {
   @Override
   public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
     resolvers.add(new TokenResolver(jwtTokenProvider));
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/*")
+        .allowedOrigins("http://localhost:3000")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("")
+        .allowCredentials(true);
   }
 }
