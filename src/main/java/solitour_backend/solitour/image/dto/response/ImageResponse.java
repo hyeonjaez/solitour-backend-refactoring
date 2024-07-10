8be61ecfb28677ea853a03065bbce0bb8e20bc98
hyeonjaez <jhjh0022@naver.com>
2024-07-11 01:18:38 +0900

    refactor: image response dto 반환 데이터 수정

--- src/main/java/solitour_backend/solitour/image/dto/response/ImageResponse.java
+++ src/main/java/solitour_backend/solitour/image/dto/response/ImageResponse.java
@@ -3,15 +3,9 @@ package solitour_backend.solitour.image.dto.response;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.time.LocalDate;
-
 @Getter
 @AllArgsConstructor
 public class ImageResponse {
-    private Long id;
     private String imageStatus;
-    private Long informationId;
-    private Long userId;
     private String address;
-    private LocalDate createdDate;
 }
