d0d16b96e4955253c640b87b6b1e809138e0ecb8
hyeonjaez <jhjh0022@naver.com>
2024-07-11 00:51:53 +0900

    feat: Information 상세 데이터의 UserPostingResponse 데이터

---
+++ src/main/java/solitour_backend/solitour/user/dto/UserPostingResponse.java
@@ -0,0 +1,11 @@
+package solitour_backend.solitour.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserPostingResponse {
+    private Long id;
+    private String name;
+}
