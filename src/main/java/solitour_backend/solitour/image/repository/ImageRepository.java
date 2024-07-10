733369c8d7680547641ed457e3ee8cc70504f44c
hyeonjaez <jhjh0022@naver.com>
2024-07-11 01:15:50 +0900

    feat: information id의 image list 반환하는 메서드

--- src/main/java/solitour_backend/solitour/image/repository/ImageRepository.java
+++ src/main/java/solitour_backend/solitour/image/repository/ImageRepository.java
@@ -2,19 +2,10 @@ package solitour_backend.solitour.image.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import solitour_backend.solitour.image.entity.Image;
-import solitour_backend.solitour.image.image_status.ImageStatus;
 
 import java.util.List;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
-    boolean existsByInformationId(Long informationId);
-
-    List<Image> findAllByInformationIdAndImageStatus(Long informationId, ImageStatus imageStatus);
-
-    Image findOneByInformationIdAndImageStatus(Long informationId, ImageStatus imageStatus);
-
-    boolean existsByUserId(Long userId);
-
-    Image findOneByUserId(Long userId);
+    List<Image> findAllByInformationId(Long informationId);
 }
