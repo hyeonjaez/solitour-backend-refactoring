a87c7b01318934ae6b0da782976ac89cbac5ae86
hyeonjaez <jhjh0022@naver.com>
2024-07-13 14:16:21 +0900

    feat: place response dto 필요 없는 id 값 제거

--- src/main/java/solitour_backend/solitour/place/dto/response/PlaceResponse.java
+++ src/main/java/solitour_backend/solitour/place/dto/response/PlaceResponse.java
@@ -9,7 +9,6 @@ import java.math.BigDecimal;
 @Getter
 @AllArgsConstructor
 public class PlaceResponse {
-    private Long id;
     private String searchId;
     private String name;
     private BigDecimal xaxis;
