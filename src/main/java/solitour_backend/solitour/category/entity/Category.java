dbb1d1538a5ed60124a4ab4b3300854109f71b93
hyeonjaez <jhjh0022@naver.com>
2024-07-13 14:15:06 +0900

    feat: 지연로딩 -> 즉시 로딩으로 수정

--- src/main/java/solitour_backend/solitour/category/entity/Category.java
+++ src/main/java/solitour_backend/solitour/category/entity/Category.java
@@ -16,7 +16,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "parent_category_id")
     private Category parentCategory;
 
