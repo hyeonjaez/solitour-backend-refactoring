7adbccb4b54d5648521ad42dccfded2d53de475e
hyeonjaez <jhjh0022@naver.com>
2024-07-13 13:48:57 +0900

    feat: GreatInformation Repository

---
+++ src/main/java/solitour_backend/solitour/great_information/repository/GreatInformationRepository.java
@@ -0,0 +1,8 @@
+package solitour_backend.solitour.great_information.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import solitour_backend.solitour.great_information.entity.GreatInformation;
+
+public interface GreatInformationRepository extends JpaRepository<GreatInformation, Long> {
+    int countByInformationId(Long informationId);
+}
