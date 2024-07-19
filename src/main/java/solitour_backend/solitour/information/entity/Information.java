8fe0fa741a26e01efe68891649a8951d69c7768f
hyeonjaez <jhjh0022@naver.com>
2024-07-19 23:46:51 +0900

    fix: import 문 에러 해결

--- src/main/java/solitour_backend/solitour/information/entity/Information.java
+++ src/main/java/solitour_backend/solitour/information/entity/Information.java
@@ -1,14 +1,7 @@
 package solitour_backend.solitour.information.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -42,7 +35,7 @@ public class Information {
   @JoinColumn(name = "user_id")
   private User user;
 
-  @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.REMOVE)
+  @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "place_id")
   private Place place;
 
