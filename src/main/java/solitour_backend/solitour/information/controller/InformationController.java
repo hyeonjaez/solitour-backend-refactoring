7b0500e24ba3dc275840fa672dfe3dac6034b006
hyeonjaez <jhjh0022@naver.com>
2024-07-21 01:20:28 +0900

    feat: 부모 카테고리, 자식 카테고리 별 1. 조회수 2. zoneCategory 별 페이지네이션 controller 및 Cookie에서 access_token 또는 refresh_token을 찾고 회원인지 판별

--- src/main/java/solitour_backend/solitour/information/controller/InformationController.java
+++ src/main/java/solitour_backend/solitour/information/controller/InformationController.java
@@ -1,18 +1,24 @@
 package solitour_backend.solitour.information.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import java.util.List;
+import java.util.Objects;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import solitour_backend.solitour.auth.support.CookieExtractor;
+import solitour_backend.solitour.auth.support.JwtTokenProvider;
 import solitour_backend.solitour.error.Utils;
 import solitour_backend.solitour.information.dto.request.InformationModifyRequest;
 import solitour_backend.solitour.information.dto.request.InformationRegisterRequest;
@@ -28,6 +34,8 @@ import solitour_backend.solitour.information.service.InformationService;
 public class InformationController {
     private final InformationService informationService;
     public static final int PAGE_SIZE = 12;
+    private final JwtTokenProvider jwtTokenProvider;
+
 
     @PostMapping
     public ResponseEntity<InformationResponse> createInformation(@Valid @RequestPart("request") InformationRegisterRequest informationRegisterRequest,
@@ -80,7 +88,10 @@ public class InformationController {
     @GetMapping("/parent-category/{parentCategoryId}")
     public ResponseEntity<Page<InformationBriefResponse>> pageInformationByParentCategory(@RequestParam(defaultValue = "0") int page,
                                                                                           @PathVariable("parentCategoryId") Long categoryId,
-                                                                                          @RequestParam(defaultValue = "0") Long userId) {
+                                                                                          HttpServletRequest request) {
+
+        Long userId = findUser(request);
+
         Pageable pageable = PageRequest.of(page, PAGE_SIZE);
         Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByParentCategory(pageable, categoryId, userId);
 
@@ -92,7 +103,9 @@ public class InformationController {
     @GetMapping("/child-category/{childCategoryId}")
     public ResponseEntity<Page<InformationBriefResponse>> pageInformationByChildCategory(@RequestParam(defaultValue = "0") int page,
                                                                                          @PathVariable("childCategoryId") Long categoryId,
-                                                                                         @RequestParam(defaultValue = "0") Long userId) {
+                                                                                         HttpServletRequest request) {
+        Long userId = findUser(request);
+
         Pageable pageable = PageRequest.of(page, PAGE_SIZE);
         Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByChildCategory(pageable, categoryId, userId);
 
@@ -105,8 +118,10 @@ public class InformationController {
     //좋아요순
     @GetMapping("/parent-category/{parentCategoryId}/like-count")
     public ResponseEntity<Page<InformationBriefResponse>> pageInformationByParentCategoryFilterLikeCount(@RequestParam(defaultValue = "0") int page,
-                                                                                                         @RequestParam(defaultValue = "0") Long userId,
-                                                                                                         @PathVariable("parentCategoryId") Long categoryId) {
+                                                                                                         @PathVariable("parentCategoryId") Long categoryId,
+                                                                                                         HttpServletRequest request) {
+        Long userId = findUser(request);
+
         Pageable pageable = PageRequest.of(page, PAGE_SIZE);
         Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByParentCategoryFilterLikeCount(pageable, categoryId, userId);
 
@@ -117,8 +132,10 @@ public class InformationController {
 
     @GetMapping("/child-category/{childCategoryId}/like-count")
     public ResponseEntity<Page<InformationBriefResponse>> pageInformationByChildCategoryFilterLikeCount(@RequestParam(defaultValue = "0") int page,
-                                                                                                        @RequestParam(defaultValue = "0") Long userId,
-                                                                                                        @PathVariable("childCategoryId") Long categoryId) {
+                                                                                                        @PathVariable("childCategoryId") Long categoryId,
+                                                                                                        HttpServletRequest request) {
+        Long userId = findUser(request);
+
         Pageable pageable = PageRequest.of(page, PAGE_SIZE);
         Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByChildCategoryFilterLikeCount(pageable, categoryId, userId);
 
@@ -129,9 +146,58 @@ public class InformationController {
 
 
     //조회순
+    @GetMapping("/parent-category/{parentCategoryId}/view-count")
+    public ResponseEntity<Page<InformationBriefResponse>> pageInformationByParentCategoryViewCount(@RequestParam(defaultValue = "0") int page,
+                                                                                                   @PathVariable("parentCategoryId") Long categoryId,
+                                                                                                   HttpServletRequest request) {
 
+        Long userId = findUser(request);
+
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+        Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByParentCategoryFilterViewCount(pageable, categoryId, userId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(briefInformationPage);
+    }
+
+    @GetMapping("/child-category/{childCategoryId}/view-count")
+    public ResponseEntity<Page<InformationBriefResponse>> pageInformationByChildCategoryViewCount(@RequestParam(defaultValue = "0") int page,
+                                                                                                  @PathVariable("childCategoryId") Long categoryId,
+                                                                                                  HttpServletRequest request) {
+        Long userId = findUser(request);
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+        Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByChildCategoryFilterViewCount(pageable, categoryId, userId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(briefInformationPage);
+    }
 
     //지역카테고리별
+    @GetMapping("/parent-category/{parentCategoryId}/zone-category/{zoneCategory}")
+    public ResponseEntity<Page<InformationBriefResponse>> pageInformationByParentCategoryAndZoneCategory(@RequestParam(defaultValue = "0") int page,
+                                                                                                         @PathVariable("parentCategoryId") Long categoryId,
+                                                                                                         @PathVariable("zoneCategory") Long zoneCategoryId,
+                                                                                                         HttpServletRequest request) {
+        Long userId = findUser(request);
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+        Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByParentCategoryAndZoneCategory(pageable, categoryId, userId, zoneCategoryId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(briefInformationPage);
+    }
+
+    @GetMapping("/child-category/{childCategoryId}/zone-category/{zoneCategory}")
+    public ResponseEntity<Page<InformationBriefResponse>> pageInformationChildCategoryAndZoneCategory(@RequestParam(defaultValue = "0") int page,
+                                                                                                      @PathVariable("childCategoryId") Long categoryId,
+                                                                                                      @PathVariable("zoneCategory") Long zoneCategoryId,
+                                                                                                      HttpServletRequest request) {
+        Long userId = findUser(request);
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+        Page<InformationBriefResponse> briefInformationPage = informationService.getBriefInformationPageByChildCategoryAndZoneCategory(pageable, categoryId, userId, zoneCategoryId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(briefInformationPage);
+    }
 
 
     @GetMapping("/ranks")
@@ -141,5 +207,20 @@ public class InformationController {
                 .status(HttpStatus.OK)
                 .body(rankInformation);
     }
+
+
+    private Long findUser(HttpServletRequest request) {
+        String token = CookieExtractor.findToken("access_token", request.getCookies());
+
+        if (Objects.isNull(token)) {
+            token = CookieExtractor.findToken("refresh_token", request.getCookies());
+        }
+        if (Objects.isNull(token)) {
+            return (long) 0;
+        }
+
+        return jwtTokenProvider.getPayload(token);
+    }
+
 }
 
