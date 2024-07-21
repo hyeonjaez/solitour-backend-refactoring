8927629a7a21b6fbfc58af1efc3d06f7253f4f9e
hyeonjaez <jhjh0022@naver.com>
2024-07-22 00:02:14 +0900

    feat: InformationModifyRequest 1. 이미지를 계속 사용한다는 request dto 필드 2. 이미지를 삭제한다는 request dto 필드

--- src/main/java/solitour_backend/solitour/information/dto/request/InformationModifyRequest.java
+++ src/main/java/solitour_backend/solitour/information/dto/request/InformationModifyRequest.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import solitour_backend.solitour.image.dto.request.ImageDeleteRequest;
+import solitour_backend.solitour.image.dto.request.ImageUseRequest;
 import solitour_backend.solitour.tag.dto.request.TagRegisterRequest;
 
 import java.util.List;
@@ -11,9 +13,14 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 public class InformationModifyRequest {
+
     @NotBlank
     @Size(min = 1, max = 50)
     private String title;
+
+    private List<ImageUseRequest> useImages;
+
+    private List<ImageDeleteRequest> deleteImages;
 
     @NotBlank
     @Size(min = 1, max = 20)
@@ -24,4 +31,5 @@ public class InformationModifyRequest {
     private String tips;
 
     private List<TagRegisterRequest> tagRegisterRequests;
+
 }
