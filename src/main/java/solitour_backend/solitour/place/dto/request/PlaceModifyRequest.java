b1ea98da1b078b12602055ccc7926ef63840123a
hyeonjaez <jhjh0022@naver.com>
2024-07-28 22:53:04 +0900

    feat: place 수정 dto bigdecimal 일때 수정, validate() 메서드 제거

--- src/main/java/solitour_backend/solitour/place/dto/request/PlaceModifyRequest.java
+++ src/main/java/solitour_backend/solitour/place/dto/request/PlaceModifyRequest.java
@@ -1,8 +1,11 @@
 package solitour_backend.solitour.place.dto.request;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
@@ -22,31 +25,17 @@ public class PlaceModifyRequest {
     @Size(min = 1, max = 30)
     private String name;
 
-    @Nullable
+    @NotNull
+    @JsonDeserialize(using = NumberDeserializers.BigDecimalDeserializer.class)
     @Digits(integer = 10, fraction = 6)
     private BigDecimal xAxis;
 
-    @Nullable
+    @NotNull
+    @JsonDeserialize(using = NumberDeserializers.BigDecimalDeserializer.class)
     @Digits(integer = 10, fraction = 6)
     private BigDecimal yAxis;
 
     @NotBlank
     @Size(min = 1, max = 50)
     private String address;
-
-    public boolean validate() {
-        boolean searchIdIsNull = this.searchId == null;
-        boolean xAxisIsNull = this.xAxis == null;
-        boolean yAxisIsNull = this.yAxis == null;
-
-        if (searchIdIsNull && (xAxisIsNull || yAxisIsNull)) {
-            return true;
-        }
-
-        if (!searchIdIsNull && (!xAxisIsNull || !yAxisIsNull)) {
-            return true;
-        }
-
-        return false;
-    }
 }
