64edff8e83c153ef58440611b78261a32730cd8e
hyeonjaez <jhjh0022@naver.com>
2024-08-04 15:47:33 +0900

    feat: validation

--- src/main/java/solitour_backend/solitour/gathering/controller/GatheringController.java
+++ src/main/java/solitour_backend/solitour/gathering/controller/GatheringController.java
@@ -11,9 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import solitour_backend.solitour.auth.config.AuthenticationPrincipal;
 import solitour_backend.solitour.error.Utils;
+import solitour_backend.solitour.error.exception.RequestValidationFailedException;
 import solitour_backend.solitour.gathering.dto.request.GatheringRegisterRequest;
 import solitour_backend.solitour.gathering.dto.response.GatheringResponse;
 import solitour_backend.solitour.gathering.service.GatheringService;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,6 +29,17 @@ public class GatheringController {
                                                              @Valid @RequestBody GatheringRegisterRequest gatheringRegisterRequest,
                                                              BindingResult bindingResult) {
         Utils.validationRequest(bindingResult);
+
+        if (gatheringRegisterRequest.getEndAge() > gatheringRegisterRequest.getStartAge()) {
+            throw new RequestValidationFailedException("시작 나이 연도가 끝 나이 연도 보다 앞에 있네요");
+        }
+        if (gatheringRegisterRequest.getScheduleStartDate().isAfter(gatheringRegisterRequest.getScheduleEndDate())) {
+            throw new RequestValidationFailedException("시작 날짜는 종료 날짜보다 앞에 있어야 합니다.");
+        }
+
+        if (gatheringRegisterRequest.getDeadline().isBefore(LocalDateTime.now())) {
+            throw new RequestValidationFailedException("마감일은 현재 시간보다 이후여야 합니다.");
+        }
 
         GatheringResponse gatheringResponse = gatheringService.registerGathering(userId, gatheringRegisterRequest);
 
