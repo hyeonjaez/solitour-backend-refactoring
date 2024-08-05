c5a75bc715398f8c7096d3b64344e455d01c09ed
hyeonjaez <jhjh0022@naver.com>
2024-08-06 02:55:04 +0900

    feat: gathering 상세 response dto

---
+++ src/main/java/solitour_backend/solitour/gathering/dto/response/GatheringDetailResponse.java
@@ -0,0 +1,45 @@
+package solitour_backend.solitour.gathering.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import solitour_backend.solitour.gathering.entity.AllowedSex;
+import solitour_backend.solitour.gathering_applicants.dto.response.GatheringApplicantsResponse;
+import solitour_backend.solitour.place.dto.response.PlaceResponse;
+import solitour_backend.solitour.tag.dto.response.TagResponse;
+import solitour_backend.solitour.user.dto.UserPostingResponse;
+import solitour_backend.solitour.zone_category.dto.response.ZoneCategoryResponse;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GatheringDetailResponse {
+    private String title;
+    private String content;
+    private Integer personCount;
+    private Integer viewCount;
+    private LocalDateTime createdAt;
+
+    private LocalDateTime scheduleStartDate;
+    private LocalDateTime scheduleEndDate;
+    private LocalDateTime deadline;
+    private Boolean isFinish;
+
+    private AllowedSex allowedSex;
+    private Integer startAge;
+    private Integer endAge;
+
+    private List<TagResponse> tagResponses;
+
+    private UserPostingResponse userPostingResponse;
+    private PlaceResponse placeResponse;
+    private ZoneCategoryResponse zoneCategoryResponse;
+
+    private Integer likeCount;
+    private Integer nowPersonCount;
+
+    private List<GatheringApplicantsResponse> gatheringApplicantsResponses;
+
+    private List<GatheringBriefResponse> gatheringRecommend;
+}
