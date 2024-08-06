2309009317e0a54fb2a250d9297c6f4189ab0f7b
hyeonjaez <jhjh0022@naver.com>
2024-08-07 02:43:29 +0900

    feat: gathering 허용인원이 이미 다 찼다는 예외 클래스

---
+++ src/main/java/solitour_backend/solitour/gathering_applicants/exception/GatheringApplicantsAlreadyFullPeopleException.java
@@ -0,0 +1,7 @@
+package solitour_backend.solitour.gathering_applicants.exception;
+
+public class GatheringApplicantsAlreadyFullPeopleException extends RuntimeException {
+    public GatheringApplicantsAlreadyFullPeopleException(String message) {
+        super(message);
+    }
+}
