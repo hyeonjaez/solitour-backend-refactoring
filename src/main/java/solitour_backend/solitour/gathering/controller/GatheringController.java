259f45fcabf6a4a093645137ca0f47141fc4a89c
hyeonjaez <jhjh0022@naver.com>
2024-08-17 23:23:36 +0900

    feat: 모임 페이지네이션 필터링 및 정렬 controller

--- src/main/java/solitour_backend/solitour/gathering/controller/GatheringController.java
+++ src/main/java/solitour_backend/solitour/gathering/controller/GatheringController.java
@@ -2,29 +2,32 @@ package solitour_backend.solitour.gathering.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+
 import java.time.LocalDateTime;
 import java.util.Objects;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import solitour_backend.solitour.auth.config.AuthenticationPrincipal;
 import solitour_backend.solitour.auth.support.CookieExtractor;
 import solitour_backend.solitour.auth.support.JwtTokenProvider;
 import solitour_backend.solitour.error.Utils;
 import solitour_backend.solitour.error.exception.RequestValidationFailedException;
 import solitour_backend.solitour.gathering.dto.request.GatheringModifyRequest;
+import solitour_backend.solitour.gathering.dto.request.GatheringPageRequest;
 import solitour_backend.solitour.gathering.dto.request.GatheringRegisterRequest;
+import solitour_backend.solitour.gathering.dto.response.GatheringBriefResponse;
 import solitour_backend.solitour.gathering.dto.response.GatheringDetailResponse;
 import solitour_backend.solitour.gathering.dto.response.GatheringResponse;
 import solitour_backend.solitour.gathering.service.GatheringService;
+
+import static solitour_backend.solitour.information.controller.InformationController.PAGE_SIZE;
 
 @RestController
 @RequiredArgsConstructor
@@ -76,6 +79,7 @@ public class GatheringController {
         if (gatheringModifyRequest.getEndAge() > gatheringModifyRequest.getStartAge()) {
             throw new RequestValidationFailedException("시작 나이 연도가 끝 나이 연도 보다 앞에 있네요");
         }
+
         if (gatheringModifyRequest.getScheduleStartDate().isAfter(gatheringModifyRequest.getScheduleEndDate())) {
             throw new RequestValidationFailedException("시작 날짜는 종료 날짜보다 앞에 있어야 합니다.");
         }
@@ -89,6 +93,23 @@ public class GatheringController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(gatheringResponse);
+    }
+
+
+    @GetMapping
+    public ResponseEntity<Page<GatheringBriefResponse>> pageGathering(@RequestParam(defaultValue = "0") int page,
+                                                                      @Valid @ModelAttribute GatheringPageRequest gatheringPageRequest,
+                                                                      BindingResult bindingResult,
+                                                                      HttpServletRequest request) {
+        Utils.validationRequest(bindingResult);
+        Long userId = findUser(request);
+
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE);
+        Page<GatheringBriefResponse> pageGathering = gatheringService.getPageGathering(pageable, gatheringPageRequest, userId);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(pageGathering);
     }
 
 
