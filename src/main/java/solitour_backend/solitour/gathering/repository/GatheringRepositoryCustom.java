60d065c9986fa48b75090d84d2396dfe60e3e976
hyeonjaez <jhjh0022@naver.com>
2024-08-17 23:22:46 +0900

    feat: 모임 페이지네이션 메서드 선언

--- src/main/java/solitour_backend/solitour/gathering/repository/GatheringRepositoryCustom.java
+++ src/main/java/solitour_backend/solitour/gathering/repository/GatheringRepositoryCustom.java
@@ -1,10 +1,20 @@
 package solitour_backend.solitour.gathering.repository;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.NoRepositoryBean;
+import solitour_backend.solitour.gathering.dto.request.GatheringPageRequest;
 import solitour_backend.solitour.gathering.dto.response.GatheringBriefResponse;
 
 @NoRepositoryBean
 public interface GatheringRepositoryCustom {
+    String LIKE_COUNT_SORT = "likes";
+    String VIEW_COUNT_SORT = "views";
+
     List<GatheringBriefResponse> getGatheringRecommend(Long informationId, Long gatheringCategoryId, Long userId);
+
+    Page<GatheringBriefResponse> getGatheringPageFilterAndOrder(Pageable pageable, GatheringPageRequest gatheringPageRequest, Long userId);
+
 }
