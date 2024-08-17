8f883e646b6b5715677c82ff26b7e9e433316f6c
hyeonjaez <jhjh0022@naver.com>
2024-08-17 23:21:52 +0900

    feat: GatheringCategory Response dto

---
+++ src/main/java/solitour_backend/solitour/gathering_category/dto/response/GatheringCategoryResponse.java
@@ -0,0 +1,11 @@
+package solitour_backend.solitour.gathering_category.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GatheringCategoryResponse {
+    private Long id;
+    private String name;
+}
