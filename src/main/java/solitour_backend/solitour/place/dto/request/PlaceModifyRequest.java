a237f282fd132d3f3ea41a26a5f9fd19bac2ac9c
hyeonjaez <jhjh0022@naver.com>
2024-08-26 15:29:06 +0900

    fix: @NotNull 추가

--- src/main/java/solitour_backend/solitour/place/dto/request/PlaceModifyRequest.java
+++ src/main/java/solitour_backend/solitour/place/dto/request/PlaceModifyRequest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.deser.std.NumberDeserializers;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Getter;
