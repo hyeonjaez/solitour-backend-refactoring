b6a774d8176b6baa6afbbd3a6abefb66ad9a782e
hyeonjaez <jhjh0022@naver.com>
2024-09-02 14:44:32 +0900

    fix: createdDate 칼럼 auto 생성 설정 문제 해결

--- src/main/java/solitour_backend/solitour/information/entity/Information.java
+++ src/main/java/solitour_backend/solitour/information/entity/Information.java
@@ -1,21 +1,13 @@
 package solitour_backend.solitour.information.entity;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import solitour_backend.solitour.category.entity.Category;
 import solitour_backend.solitour.place.entity.Place;
 import solitour_backend.solitour.user.entity.User;
@@ -26,6 +18,7 @@ import solitour_backend.solitour.zone_category.entity.ZoneCategory;
 @Setter
 @Table(name = "information")
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Information {
 
     @Id
