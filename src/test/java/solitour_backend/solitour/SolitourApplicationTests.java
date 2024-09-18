4388b136e5c22bcb70e6cdc9c06bea75b9b08f75
hyeonjaez <jhjh0022@naver.com>
2024-09-18 15:42:05 +0900

    fix: 충돌 해결

--- src/test/java/solitour_backend/solitour/SolitourApplicationTests.java
+++ src/test/java/solitour_backend/solitour/SolitourApplicationTests.java
@@ -1,12 +1,10 @@
 package solitour_backend.solitour;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @ActiveProfiles("test")
 class SolitourApplicationTests {
 
