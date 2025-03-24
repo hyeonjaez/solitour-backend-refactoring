e916819568ff2ae0413373579423c22187c8e667
hyeonjaez <jhjh0022@naver.com>
2025-03-24 13:51:53 +0900

    feat: user response dto

---
+++ src/main/java/solitour_backend/solitour/user/dto/UserResponse.java
@@ -0,0 +1,17 @@
+package solitour_backend.solitour.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * packageName    : solitour_backend.solitour.user.dto<br>
+ * fileName       : UserResponse.java<br>
+ * author         : Fiat_lux<br>
+ * date           : 2025-03-24<br>
+ * description    : User response dto 클래스입니다. <br>
+ */
+@Getter
+@AllArgsConstructor
+public class UserResponse {
+    private Long userId;
+}
