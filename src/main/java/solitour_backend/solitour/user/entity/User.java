d000b2414bd92ef072781bd1c5e8b34298839abe
hyeonjaez <jhjh0022@naver.com>
2025-03-24 13:50:05 +0900

    feat: Sex enum type data

--- src/main/java/solitour_backend/solitour/user/entity/User.java
+++ src/main/java/solitour_backend/solitour/user/entity/User.java
@@ -1,11 +1,6 @@
 package solitour_backend.solitour.user.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
@@ -37,7 +32,8 @@ public class User {
     private Integer age;
 
     @Column(name = "user_sex")
-    private String sex;
+    @Enumerated(EnumType.STRING)
+    private Sex sex;
 
     @Column(name = "user_email")
     private String email;
