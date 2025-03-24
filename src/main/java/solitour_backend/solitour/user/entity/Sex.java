6450904dfcb0432add48fdf5ba97d23688dbad5e
hyeonjaez <jhjh0022@naver.com>
2025-03-24 13:49:47 +0900

    feat: Sex enum class

---
+++ src/main/java/solitour_backend/solitour/user/entity/Sex.java
@@ -0,0 +1,12 @@
+package solitour_backend.solitour.user.entity;
+
+/**
+ * packageName    : solitour_backend.solitour.user.entity<br>
+ * fileName       : Sex.java<br>
+ * author         : Fiat_lux<br>
+ * date           : 2025-03-24<br>
+ * description    :  성별 enum class 입니다.<br>
+ */
+public enum Sex {
+    MALE, FEMALE
+}
