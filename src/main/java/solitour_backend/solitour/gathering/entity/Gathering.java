29ff7cbd935ae9406febf27731280d7e7905ec41
hyeonjaez <jhjh0022@naver.com>
2025-03-26 17:52:12 +0900

    feat: 낙관적 락 적용으로 버전 칼럼 추가

--- src/main/java/solitour_backend/solitour/gathering/entity/Gathering.java
+++ src/main/java/solitour_backend/solitour/gathering/entity/Gathering.java
@@ -1,16 +1,6 @@
 package solitour_backend.solitour.gathering.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
@@ -101,6 +91,10 @@ public class Gathering {
 
     @Column(name = "gathering_open_chatting_url")
     private String openChattingUrl;
+
+    @Version
+    @Column(name = "version")
+    private Long version;
 
     public Gathering(User user, ZoneCategory zoneCategory, GatheringCategory gatheringCategory, Place place,
                      String title, String content, Integer personCount, Integer viewCount,
